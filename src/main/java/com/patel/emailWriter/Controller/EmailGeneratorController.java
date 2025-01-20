e11b1ea71ab46228672da5a4c29f7221298661fd
Lalit Patel <89211719+lalit-patel@users.noreply.github.com>
2025-01-20 22:49:15 +0530

    Frontend added

--- src/main/java/com/patel/emailWriter/Controller/EmailGeneratorController.java
+++ src/main/java/com/patel/emailWriter/Controller/EmailGeneratorController.java
@@ -4,14 +4,12 @@ import com.patel.emailWriter.DTO.EmailRequest;
 import com.patel.emailWriter.Service.EmailGeneratorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
